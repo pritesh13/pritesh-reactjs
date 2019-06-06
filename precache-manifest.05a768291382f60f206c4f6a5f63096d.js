71cf3a8db284b8200fa6e0ad88fad3e6b6af73d4
Pritesh <pritesh@logisticinfotech.com>
2019-06-06 12:08:31 +0530

    Updates

---
+++ precache-manifest.05a768291382f60f206c4f6a5f63096d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b5db2c66b1340363e445f073c544c4",
+    "url": "/pritesh-reactjs/index.html"
+  },
+  {
+    "revision": "f66bc64b569b042164b3",
+    "url": "/pritesh-reactjs/static/css/main.cfd6d18d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/pritesh-reactjs/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f66bc64b569b042164b3",
+    "url": "/pritesh-reactjs/static/js/main.4f145c52.chunk.js"
+  },
+  {
+    "revision": "126ce353177cfce7bc4e",
+    "url": "/pritesh-reactjs/static/js/runtime~main.fdaa963b.js"
+  }
+]);
